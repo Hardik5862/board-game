d24b3ad287d4a403f645bfd91537c0a14ec5d0db
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-12-13 11:47:42 +0530

    initial game setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="bg">
+      <h1>Board Game</h1>
+      <div className="board"></div>
+    </main>
+  );
+}
+
+export default App;
